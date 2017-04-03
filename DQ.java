6f7b163a99746bf3a29fa568b394a5c3e4c9bc22
Alitquan <amallick@stuy.edu>
2017-04-03 19:21:04 -0400

    Debugged methods and commented in information, added size method

--- DQ.java
+++ DQ.java
@@ -1,5 +1,9 @@
 public class DQ <T> implements Deque<T>{
-    
+    /*
+      all of the methods have linear runtimes-- with 
+      the exception of toString which has a linear
+      runtime
+    */
     private DLLNode <T> _front, _end;
     private int _size;
     
@@ -10,14 +14,14 @@ public class DQ <T> implements Deque<T>{
     
     //pop & push: STACK PROPERTIES- FILO
     public T pop(){
-	return this.removeLast();
+	return this.pollLast();
     }
     public void push(T x){
         this.addLast(x);
     }
 
-    //remove-retrieves and removes the head of the queue (first element) 
-    public T remove() {
+    //retrieves and removes the _front of the queue (first element); returns null if empty
+    public T poll() {
 	if (this.isEmpty()) {
 	    return null;
 	}
@@ -30,8 +34,9 @@ public class DQ <T> implements Deque<T>{
 	}
     }
 	    
-	    
-    public T removeFirst(){
+
+    //same functionality as remove; return null if empty
+    public T pollFirst(){
 	if (this.isEmpty()) {
 	    return null;
 	}
@@ -44,7 +49,8 @@ public class DQ <T> implements Deque<T>{
 	}
     }
 
-    public T removeLast(){
+    //retrieves and removes the _end of the queue (last element); returns null if empty
+    public T pollLast(){
 	if (this.isEmpty()) {
 	    return null;
 	}
@@ -57,18 +63,19 @@ public class DQ <T> implements Deque<T>{
 	}
     }
 	
-    //add-inserts into queue (tail)
+    //inserts into _end of queue
     public void add(T x){
 	if ( isEmpty() ) {
 	    _front = _end = new DLLNode<T>(x,null,null );
 	}
-	else{
-	DLLNode<T> holder=new DLLNode<T>(x,_end,null);
-	_end=holder;
+	else {
+	    _end.setNext(new DLLNode<T>(x,_end,null));
+	    _end = _end.getNext();
 	}
 	_size++;
     }
-    
+
+    // inserts into _front of queue
     public void addFirst(T x){
 	if ( isEmpty() ) {
 	    _front = _end = new DLLNode<T>(x,null,null );
@@ -79,26 +86,28 @@ public class DQ <T> implements Deque<T>{
 	}
 	_size++;
     }
-    
+
+    // same functionality as add
     public void addLast(T x){
 	if ( isEmpty() ) {
 	    _front = _end = new DLLNode<T>(x,null, null );
 	}
 	else{
-	DLLNode<T> holder=new DLLNode<T>(x,_end,null);
-	_end=holder;
+	     _end.setNext(new DLLNode<T>(x,_end,null));
+	    _end = _end.getNext();
 	}
 	_size++;
     }
     
-    //peek
+    //returns cargo of _front of deque
     public T peekFirst(){
 	if (this.isEmpty()) {
 	    return null;
 	}
 	return _front.getCargo();
     }
-    
+
+    //returns cargo of _back of deque
     public T peekLast(){
 	if (this.isEmpty()) {
 	    return null;
@@ -106,17 +115,26 @@ public class DQ <T> implements Deque<T>{
 	return _end.getCargo();
     }
 
+    // returns true if deque is empty, false otherwise
     public boolean isEmpty() {
 	return (_size == 0); 
     }
 
+    // returns size of deque
+    public int size() {
+	return _size();
+    }
+
+    // overridden toString method to print contents of deque
     public String toString(){
-	String retStr="";
+	String retStr="FRONT----";
 	DLLNode<T> trav=_front;
 	while (trav!=null){
-	    retStr+=trav.getCargo()+"";
+	    retStr+=trav.getCargo()+"<----";
 	    trav=trav.getNext();
 	}
+	retStr= retStr.substring(0,retStr.length()-5);
+	retStr+="----END";
 	return retStr;
     }
     public static void main(String[] args){
@@ -124,7 +142,14 @@ public class DQ <T> implements Deque<T>{
 	DairyQueen.add("mango");
 	DairyQueen.add("strawberry");
 	DairyQueen.add("blitz");
-
+	DairyQueen.addFirst("luscious");
+	DairyQueen.addLast("soup");
+	System.out.println(DairyQueen);
+	System.out.println("peekFirst() == " + DairyQueen.peekFirst());
+	System.out.println("peekLast() == " + DairyQueen.peekLast());
+	System.out.println("pollFirst() == " + DairyQueen.pollFirst());
+	System.out.println(DairyQueen);
+       	System.out.println("pollLast() == " + DairyQueen.pollLast());
 	System.out.println(DairyQueen);
     }
 	
--- Deque.java
+++ Deque.java
@@ -4,8 +4,9 @@ public interface Deque<T>{
     public void push(T x);
 
     //removing
-    public T remove();
-    public T removeFirst();
+    public T poll();
+    public T pollFirst();
+    public T pollLast();
 
     //adding
     public void add(T x);
@@ -17,4 +18,5 @@ public interface Deque<T>{
     public T peekLast();
 
     public boolean isEmpty();
+    public int size();
 }
